aedf44a5e88cb43023b2ce2ff33dd9e2a3654fff
Ankit Kumar <ankit.kumar@morningstar.com>
2023-07-13 18:10:13 +0530

    Day 1 - How Python works? & wrote the first python code

---
+++ python_basics.py
@@ -0,0 +1,8 @@
+##How does Python works?
+# Python code that we write goes to python interpreter gets converted from human readable to machine codes i.e. 0's and 1's and then gets executed using a compiler.
+
+#Here we take user's name as input store it in name variable and then print it.
+name = input("Hey, What is your Name?")
+print("Hey "+name)
+
+
